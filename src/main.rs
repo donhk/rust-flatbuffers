fe0e1261e0518f1a230f73f6eedf95589d280768
Frederick Alvarez <fralvarez@linkedin.com>
2023-05-26 19:21:44 -0500

    added more clients support

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,19 @@ use tokio::net::TcpListener;
 async fn main() {
     println!("Hello, world!");
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-    let (read, mut write) = socket.split();
-    let mut reader = BufReader::new(read);
     loop {
-        let mut line = String::new();
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 {
-            break;
-        }
-        write.write_all(&line.as_bytes()).await.unwrap();
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            let (read, mut write) = socket.split();
+            let mut reader = BufReader::new(read);
+            loop {
+                let mut line = String::new();
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                write.write_all(&line.as_bytes()).await.unwrap();
+            }
+        });
     }
 }
